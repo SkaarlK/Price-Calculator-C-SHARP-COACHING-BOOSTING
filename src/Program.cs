567dc19d6c7df89265d154a8ae651f1021c266ba
Kevin Chaves <kchaves@innovagency.com>
2024-05-07 09:42:33 +0100

    IMPROV(readability): Ternary operator usage

--- src/Program.cs
+++ src/Program.cs
@@ -9,35 +9,11 @@ class Program
     public static List<string> singleDivision = [ "I" ];
     static void Main()
    {
-        Division division;
-        Division targetDivision;
-
         Rank rank = new(0, $"Select your current rank:\n", allRanks);
-
-        bool isRankSingleDivisioned = singleDivisionRanks.Contains(rank.ToString());
-
-        if (isRankSingleDivisioned)
-        {
-            division = new(1, "Select your current division:\n", singleDivision);
-        } 
-        else
-        {
-            division = new(0, "Select your current division:\n", allDivisions);
-        }
-
+        Division division =  singleDivisionRanks.Contains(rank.ToString()) ? new(1, "Select your current division:\n", singleDivision) : new(0, "Select your current division:\n", allDivisions);
 
         Rank targetRank = new(0, $"Select your target rank:\n", allRanks);
-
-        bool isTargetRankSingleDivisioned = singleDivisionRanks.Contains(targetRank.ToString());
-
-        if (isTargetRankSingleDivisioned)
-        {
-            targetDivision = new(1, "Select your target division:\n", singleDivision);
-        }
-        else
-        {
-            targetDivision = new(0, "Select your target division:\n", allDivisions);
-        }
+        Division targetDivision = singleDivisionRanks.Contains(targetRank.ToString()) ? new(1, "Select your target division:\n", singleDivision) : new(0, "Select your target division:\n", allDivisions);
 
         AverageLeaguePoints averageLPGain = new(0, "Select your average points earned per win:\n", ["14-", "15-18", "19-24", "25-29", "30+"]);
         Server server = new(0, "Select your server:\n", ["EUW", "EUNE", "BR", "LAN", "NA"]);
